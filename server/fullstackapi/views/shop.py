1443edd14cd9ee528663a981a963f3258dfc16c6
Austin Phy <atphy42@gmail.com>
2020-12-18 17:33:12 -0600

    single shop also returns list of records

--- server/fullstackapi/views/shop.py
+++ server/fullstackapi/views/shop.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from fullstackapi.models import Shop
+from fullstackapi.models import Shop, Record
 
 
 class Shops(ViewSet):
@@ -21,6 +21,41 @@ class Shops(ViewSet):
             shops, many=True, context={'request': request})
         return Response(serializer.data)
 
+    def retrieve(self, request, pk=None):
+        """Handle GET request for single shop and records
+        Returns:
+            Response JSON serialized shop instance
+        """
+        try:
+            shop = Shop.objects.get(pk=pk)
+            records = Record.objects.filter(shop_id=shop)
+
+            records = RecordSerializer(
+                records, many=True, context={'request': request})
+            shop = ShopSerializer(
+                shop, many=False, context={'request': request})
+
+            shop_records = {}
+            shop_records["shop"] = shop.data
+            shop_records["records"] = records.data
+
+            # serializer = ShopRecordsSerializer(shop, context={'request': request})
+            return Response(shop_records)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+class RecordSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for records"""
+    class Meta:
+        model = Record
+        fields = ('id', 'name', 'artist', 'price', 'image_url', 'date_added')
+
+class ShopRecordsSerializer(serializers.ModelSerializer):
+
+    records = RecordSerializer(many=True)
+    class Meta:
+        model = Shop
+        fields = ('id', 'profile', 'verified', 'address', 'city', 'state', 'zip_code', 'contact_phone', 'contact_email')
 class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
