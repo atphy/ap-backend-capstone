b5b82c2658b6f83df4f3d06bc4c6d5e6800c0f91
Austin Phy <atphy42@gmail.com>
2020-12-22 14:50:04 -0600

    client side admin

--- fullstack-client/src/components/ApplicationViews.js
+++ fullstack-client/src/components/ApplicationViews.js
@@ -1,15 +1,89 @@
-import React from "react";
-import "./Fullstack.css";
+import React, {useEffect, useContext} from "react";
+import { Route, Redirect } from "react-router-dom"
+import { ApproveShop } from './admin/ApproveShop'
+import { Shops } from './Shops/Shops'
+import { Customers } from './Customers/Customers'
+import { UserProvider, UserContext } from './users/UserProvider'
+import { ShopProvider } from './Shops/ShopProvider'
 
+
+// if admin
+// if customer
+// if shop
+// if unauthed
 
 export const ApplicationViews = (props) => {
 
+    const { getCurrentUser, currentUser } = useContext(UserContext)
+
+    useEffect(()=>{
+        getCurrentUser()
+    }, [])
+
+//    const profileRouter = () => {
+//        if (currentUser.profile_type === 1) {
+//            return <Redirect to="/admin" />
+//        } else if (currentUser.profile_type === 2) {
+//            return <Redirect to="/home" />
+//        }
+//    }
+    
     return (
         <>
-            <main className="main-container" style={{ margin: "0 0", lineHeight: "1.75rem", }}> 
-            <h1>hi</h1>
-            
-            </main>
-        </>
+        <Route render={() => {
+            if (currentUser.profile_type === 1) {
+
+                return <Redirect to="/admin" />
+
+            } else if (currentUser.profile_type === 2) {
+                
+                return <Redirect to="/myshop" />
+
+            } else if (currentUser.profile_type === 3) {
+                
+                return <Redirect to="/home" />
+            }
+        }} />
+
+        <Route path="/admin" render={(props) => {
+            if (currentUser.profile_type !== 1) {
+                return <Redirect to="/" />
+            } else {
+                return (
+                <>
+                <ShopProvider>
+                    <ApproveShop {...props} />
+                </ShopProvider>
+                </>
+                )
+            }
+        }} />
+
+        <Route path="/myshop" render={(props) => {
+            if (currentUser.profile_type !== 2) {
+                return <Redirect to="/" />
+            } else {
+                return (
+                <>
+                <UserProvider>
+                    <Shops {...props} />
+                </UserProvider>
+                </>
+                )
+            }
+        }} />
+
+        <Route path="/home" render={(props) => {
+            return (
+            <>
+            <UserProvider>
+                <Customers {...props} />
+            </UserProvider>
+            </>
+            )
+        }} />
+        </>  
     )
+
+    
 };
--- fullstack-client/src/components/Customers/Customers.js
+++ fullstack-client/src/components/Customers/Customers.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const ShopVerification = (props) => {
+export const Customers = (props) => {
 
     return (
         <>
             <main className="main-container" style={{ margin: "0 0", lineHeight: "1.75rem", }}> 
-            <h1>shop being approved</h1>
+            <h1>hi customer</h1>
             
             </main>
         </>
